9f8b7942041366258af0eb02ac4c4d010ccd96b5
Mohamed Faiz <120184609+fxyz-ai@users.noreply.github.com>
2023-08-17 19:26:14 +0530

    Create jsassignment1.js

---
+++ jsassignment1.js
@@ -0,0 +1,10 @@
+function calculateSum(a, b) {
+    return a + b;
+}
+
+// Example usage
+const num1 = 5;
+const num2 = 7;
+const sum = calculateSum(num1, num2);
+
+console.log(`The sum of ${num1} and ${num2} is: ${sum}`);
